f53bfe5b068325e9c261d2dd30baa0844120490a
pspgt <gabry.t92@gmail.com>
2016-02-29 20:10:41 +0100

    Aggiunti accensione e spegnimento di un led via BLE

--- app/src/main/java/it/pspgt/ble/testingble/MainActivity.java
+++ app/src/main/java/it/pspgt/ble/testingble/MainActivity.java
@@ -2,7 +2,6 @@ package it.pspgt.ble.testingble;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGatt;
 import android.bluetooth.BluetoothGattCharacteristic;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
@@ -10,8 +9,8 @@ import android.bluetooth.le.ScanFilter;
 import android.bluetooth.le.ScanSettings;
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -24,13 +23,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private BluetoothLeScanner scanner;
     private List<BluetoothGattCharacteristic> caratteristiche;
     private ScanLE scannerCallback;
-    private Button b;
+    private Button b, b1, b2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         b=(Button) findViewById(R.id.button);
         b.setOnClickListener(this);
+        b1 = (Button) findViewById(R.id.button2);
+        b1.setOnClickListener(this);
+        b2 = (Button) findViewById(R.id.button3);
+        b2.setOnClickListener(this);
         bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
         if (bluetoothAdapter == null || !bluetoothAdapter.isEnabled()) {
@@ -56,22 +59,58 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-        while(!scannerCallback.isStatus()){
+        if (v.getId() == b.getId()) {
+            while (!scannerCallback.isStatus()) {
 
+            }
+            BluetoothDevice device = scannerCallback.getDevice();
+            GattCallback gattCallback = new GattCallback();
+            device.connectGatt(this, true, gattCallback);
+            while (!gattCallback.isReady()) {
+
+            }
+            caratteristiche = gattCallback.getCaratteristiche();
+            byte prova[] = new byte[32];
+            prova = "Stringa da inviare\0".getBytes();
+
+            caratteristiche.get(0).setValue(prova);
+            gattCallback.getGatt().writeCharacteristic(caratteristiche.get(0));
+
+            //   gattCallback.getGatt().disconnect();
+        } else if (v.getId() == b1.getId()) {
+            while (!scannerCallback.isStatus()) {
+
+            }
+            BluetoothDevice device = scannerCallback.getDevice();
+            GattCallback gattCallback = new GattCallback();
+            device.connectGatt(this, true, gattCallback);
+            while (!gattCallback.isReady()) {
+
+            }
+            caratteristiche = gattCallback.getCaratteristiche();
+            byte prova[] = new byte[1];
+            prova[0] = (byte) '1';
+
+            caratteristiche.get(1).setValue(prova);
+            gattCallback.getGatt().writeCharacteristic(caratteristiche.get(1));
+
+        } else {
+            while (!scannerCallback.isStatus()) {
+
+            }
+            BluetoothDevice device = scannerCallback.getDevice();
+            GattCallback gattCallback = new GattCallback();
+            device.connectGatt(this, true, gattCallback);
+            while (!gattCallback.isReady()) {
+
+            }
+            caratteristiche = gattCallback.getCaratteristiche();
+            byte prova[] = new byte[1];
+            prova[0] = (byte) '0';
+
+            caratteristiche.get(1).setValue(prova);
+            gattCallback.getGatt().writeCharacteristic(caratteristiche.get(1));
         }
-        BluetoothDevice device=scannerCallback.getDevice();
-        GattCallback gattCallback=new GattCallback();
-        device.connectGatt(this,true,gattCallback);
-        while(!gattCallback.isReady()){
-
-        }
-        caratteristiche=gattCallback.getCaratteristiche();
-        byte prova[]=new byte[32];
-        prova="Stringa da inviare\0".getBytes();
-
-        caratteristiche.get(0).setValue(prova);
-        gattCallback.getGatt().writeCharacteristic(caratteristiche.get(0));
-
-     //   gattCallback.getGatt().disconnect();
     }
+
 }
